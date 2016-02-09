0a211d23d1dd010bba3e5b705e5ed40bed390142
oakes <zsoakes@gmail.com>
2016-02-09 11:09:27 -0500

    Create animal classes

---
+++ src/Dog.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zach on 2/9/16.
+ */
+public class Dog extends Mammal {
+    public Dog() {
+        this.name = "Dog";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+}
---
+++ src/Hawk.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zach on 2/9/16.
+ */
+public class Hawk extends Bird {
+    public Hawk() {
+        this.name = "Hawk";
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Cawww");
+    }
+}
